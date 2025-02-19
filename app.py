17055e0f425a03b03040b1ed62204f837358019e
Caifu Lin <105020004+AugLin@users.noreply.github.com>
2025-02-18 21:32:13 -0500

    testing on auto delivery

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Testing auto delivery</p>"
+    return "<h1>Testing auto delivery, new update</p>"
 
 @app.route('/about')
 def about():
